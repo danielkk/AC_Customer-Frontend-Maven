75918522195af50dd3b17f291d65dfcee2cc1656
Anders <Anders@BATTLESTATION>
2016-01-04 12:17:21 +0100

    SimpleGUI - skal bare implementeres

--- src/main/java/UI/SimpleGUI.java
+++ src/main/java/UI/SimpleGUI.java
@@ -7,7 +7,6 @@ package UI;
 
 import Persistence.DummyRepository;
 import contract.dto.reservation.ReservationDetail;
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -26,47 +25,53 @@ public class SimpleGUI {
         Scanner scan = new Scanner(System.in);
         boolean runBool = true;
         System.out.println("Welcome To The Ferry Reservation System!");
-        System.out.println("Please choose an action..");
-        System.out.println("Type:\n\"create\" to create a reservation\n\"cancel\" to cancel a reservation\n\"lookup\" to lookup a reservation\n\"quit\" to close the program");
+        System.out.println("\nPlease choose an action..");
+        System.out.println("\"create\" to create a reservation\n\"cancel\" to cancel a reservation\n\"lookup\" to lookup a reservation\n\"quit\" to close the program");
         while (runBool) {
+            
             String action = scan.nextLine();
-            if ("create".equals(action)) {
-                System.out.println("You chose CREATE. Follow the instructions to make a reservation!");
-                long id = dr.getReservations().size() + 1;
-                Date date = new Date();
-                double price = 49.95;
-                System.out.println("Type of ticket:\n\"adult\" to reservate adult ticket\n\"child\" to reservate child ticket");
-                String ticketType = scan.nextLine();
-                System.out.println("Insert your first name:");
-                String firstName = scan.nextLine();
-                System.out.println("Insert your last name:");
-                String lastName = scan.nextLine();
-                System.out.println("Insert your email:");
-                String email = scan.nextLine();
-                int routeId = 2;
-                ReservationDetail rd = new ReservationDetail(id, date, price, ticketType, firstName, lastName, email, routeId);
-                System.out.println("Reservation succesfully made!");
-                System.out.println("Your reservation ID is " + id);
-            } else if ("cancel".equals(action)) {
-                System.out.println("To cancel a reservation, insert the reservation ID:");
-                long id = scan.nextLong();
-                try {
-                    dr.cancelReservation(id);
-                } catch (Exception ex) {
-                    Logger.getLogger(SimpleGUI.class.getName()).log(Level.SEVERE, null, ex);
+            if (null != action) switch (action) {
+                case "create":{
+                    long id = dr.getReservations().size() + 1;
+                    Date date = new Date();
+                    double price = 49.95;
+                    System.out.println("\nType of ticket:\n\"adult\" to reservate adult ticket\n\"child\" to reservate child ticket");
+                    String ticketType = scan.nextLine();
+                    System.out.println("\nInsert your first name:");
+                    String firstName = scan.nextLine();
+                    System.out.println("\nInsert your last name:");
+                    String lastName = scan.nextLine();
+                    System.out.println("\nInsert your email:");
+                    String email = scan.nextLine();
+                    int routeId = 2;
+                    dr.createReservation(id, date, price, ticketType, firstName, lastName, email, routeId);
+                    System.out.println("\nReservation succesfully made!");
+                    System.out.println("Your reservation ID is " + id);
+                    break;
                 }
-            } else if ("lookup".equals(action)) {
-                System.out.println("To lookup a reservation, insert the reservation ID:");
-                long id = scan.nextLong();
-                try {
-                    ReservationDetail rd = dr.getReservation(id);
-                    System.out.println("Date: " + rd.getDate().toString() + "\nTicket type: " + rd.getTicketType() + "\nFirst name: " + rd.getFirstName() + "\nLast name: " + rd.getLastName() + "\nEmail: " + rd.getEmail() + "\nRoute ID: " + rd.getRouteId());
-                } catch (Exception ex) {
-                    Logger.getLogger(SimpleGUI.class.getName()).log(Level.SEVERE, null, ex);
+                case "cancel":{
+                    System.out.println("\nTo cancel a reservation, insert the reservation ID:");
+                    long id = scan.nextLong();
+                    try {
+                        dr.cancelReservation(id);
+                    } catch (Exception ex) {
+                        System.out.println("No reservation found for id: "+id);
+                    }       break;
                 }
-            } else if ("quit".equals(action)) {
-                System.out.println("Exiting application..");
-                runBool = false;
+                case "lookup":{
+                    System.out.println("\nTo lookup a reservation, insert the reservation ID:");
+                    long id = scan.nextLong();
+                    try {
+                        ReservationDetail rd = dr.getReservation(id);
+                        System.out.println("Date: " + rd.getDate().toString() + "\nTicket type: " + rd.getTicketType() + "\nFirst name: " + rd.getFirstName() + "\nLast name: " + rd.getLastName() + "\nEmail: " + rd.getEmail() + "\nRoute ID: " + rd.getRouteId());
+                    } catch (Exception ex) {
+                        System.out.println("No reservation found for id: "+id);
+                    }       break;
+                }
+                case "quit":
+                    System.out.println("Exiting application..");
+                    runBool = false;
+                    break;
             }
         }
     }
